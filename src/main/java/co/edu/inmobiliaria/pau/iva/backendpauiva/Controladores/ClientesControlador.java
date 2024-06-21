b58a73e7c285eec71a94d6b09540aa521075c397
Ivan <isierra@uniempresarial.edu.co>
2024-06-21 12:55:18 -0500

    Otro commit

--- src/main/java/co/edu/inmobiliaria/pau/iva/backendpauiva/Controladores/ClientesControlador.java
+++ src/main/java/co/edu/inmobiliaria/pau/iva/backendpauiva/Controladores/ClientesControlador.java
@@ -1,22 +1,14 @@
 package co.edu.inmobiliaria.pau.iva.backendpauiva.Controladores;
 
-import co.edu.inmobiliaria.pau.iva.backendpauiva.Dominio.Clientes;
 import co.edu.inmobiliaria.pau.iva.backendpauiva.Servicios.ClientesService;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
-@RequestMapping({"/clientes"})
+@RequestMapping({ "/clientes" })
 public class ClientesControlador {
     @Autowired
     ClientesService service;
